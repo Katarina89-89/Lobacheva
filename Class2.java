face8fe61ec9700a6ddc0d5563d59b6255a8e7ec
Katarina89-89 <161703457+Katarina89-89@users.noreply.github.com>
2024-03-01 21:49:29 +0400

    Add files via upload

---
+++ Class2.java
@@ -0,0 +1,19 @@
+public class Class2 {public static void main(String[] args) {
+    int a = 2;
+    int b = 7;
+
+    System.out.println(a+b);
+    System.out.println(b-a);
+    System.out.println(a*b);
+    System.out.println(b/a);
+
+    if (a > b) {
+        System.out.println("a > b");
+    } else if (a < b) {
+        System.out.println("a < b");
+    } else {
+        System.out.println("a = b");
+
+
+
+}}}
